a70815fd7d4ff06d70917195832c2fd4679931b9
snowypy <git@snowyjs.lol>
2024-11-27 01:28:29 +0000

    Font changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,44 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import React from "react"
+import localFont from 'next/font/local';
 import { Toaster } from "@/components/ui/toaster"
-import Link from "next/link";
-import {Package, Github, Menu, Mail, Target, DollarSign, Server, FileText} from 'lucide-react';
-import {Button} from "@/components/ui/button";
 import {AuthProvider} from "@/context/auth-context";
 import {Header} from "@/components/header";
 import {Footer} from "@/components/footer";
-
-const inter = Inter({ subsets: ['latin'] })
-
 export const metadata: Metadata = {
   title: 'ByteStore',
   description: 'Build and serve Java artifacts from GitHub repositories with ease',
 }
 
+const monaspacekrypton = localFont({
+    src: [
+        {
+            path: '../public/fonts/MonaspaceNeon-Regular.woff',
+            weight: '400',
+            style: 'normal',
+        }
+    ],
+    variable: '--font-krypton'
+});
+
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-      <html lang="en">
-      <body className={inter.className}>
-      <AuthProvider>
-          <div className="flex flex-col min-h-screen bg-background text-foreground">
-              <Header></Header>
-              {children}
-              <Footer></Footer>
-          </div>
-      </AuthProvider>
-      <Toaster/>
-      </body>
-      </html>
-  )
+    return (
+        <html lang="en">
+        <body className={monaspacekrypton.className}>
+            <AuthProvider>
+                <div className="flex flex-col min-h-screen bg-background text-foreground">
+                    <Header />
+                    {children}
+                    <Footer />
+                </div>
+            </AuthProvider>
+            <Toaster />
+        </body>
+        </html>
+    )
 }
